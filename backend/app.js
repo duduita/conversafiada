fca282cedb4210cc36b3d18028b77026811c5494
Eduardo Menezes Moraes <eduardomenezesm@msn.com>
2020-10-21 22:46:08 -0300

    bug solved

--- backend/app.js
+++ backend/app.js
@@ -59,6 +59,7 @@ app.use(function(req, res, next) {
 // Routes
 app.use("/", require("./routes/index.js"));
 app.use("/users", require("./routes/users.js"));
+app.use("/message", require("./routes/message.js"));
 app.use(express.static("public"));
 
 io.on("connection", function(socket) {
